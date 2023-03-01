32e91802c8b24d5acdd864a5f872acc84899a843
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-01 23:34:05 +0530

    Completed Chapter 2

---
+++ C Chapterwise Answers/Answer - Chapter 2/q3.c
@@ -0,0 +1,12 @@
+//divisibility without if else
+#include<stdio.h>
+int main() {
+    int a;
+
+    printf("Type the value - ");
+    scanf("%d",&a);
+
+    printf("Divisibility test returns : %d\n",97%a);
+
+    return 0;
+}
---
+++ C Chapterwise Answers/Answer - Chapter 2/q4.c
@@ -0,0 +1,10 @@
+// answer ->  (-2)
+#include<stdio.h>
+int main() {
+    int a;
+    a= 3*2/3-3+1;
+
+
+    printf("The area of reactangle is %d unit square", a);
+    return 0;
+}
