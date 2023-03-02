e314e7f57db1268ff049b52a431ba5205eb00093
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-02 22:29:20 +0530

    Update q1.c

--- C Chapterwise Answers/Answers - Chapter 3/q1.c
+++ C Chapterwise Answers/Answers - Chapter 3/q1.c
@@ -1,17 +1,6 @@
-#include<stdio.h>
-int main(){
-    int score,count,i=0;
-    repeat:
-        printf("enter the score of the player:");
-        scanf("%d",&score);
-        if(score>=100){
-            count++;
-        }
-        i++;
-        if(i<=11){
-            goto repeat;
-        }
-    printf("the no. of player scored century is: %d",count);
-    return 0;
+#include <stdio.h>
 
+int main(){
+    
+    return 0;
 }
