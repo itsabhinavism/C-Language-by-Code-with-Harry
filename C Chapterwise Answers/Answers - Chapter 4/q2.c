a1885a716caec4b1fdbfdfeba1ab1ae05dbaea85
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-03 19:10:37 +0530

    update

---
+++ C Chapterwise Answers/Answers - Chapter 4/q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float main(){
+    float a,b;
+    printf("Type the value - ");
+    scanf("%f",&b);
+
+    for(a=1;a<10;a++) {
+        printf("%f x %f = %f\n", b,a,a*b);
+    }
+    return 0;
+}
