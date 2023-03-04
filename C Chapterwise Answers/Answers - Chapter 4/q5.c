6607784468d2036b29fbdafa21da012522424af5
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-04 19:19:52 +0530

    update

--- C Chapterwise Answers/Answers - Chapter 4/q5.c
+++ C Chapterwise Answers/Answers - Chapter 4/q5.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 float  main() {
     float a,b;
+    // float a=0; 
     printf("Type the value - ");
     scanf("%f",&b);
 
-    for (a=0;a<=11;a++) {
-        printf("%f + %f = %f\n",b,a,(a + b));
+    while(a<=11) {
+        printf("%f + %f = %f\n",b,a,(a+b)); a ++;
     }
+
     return 0;
 }
