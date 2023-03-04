96bc3af4fd05a85214f6d47a00f99f05fed43ae6
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-04 21:15:51 +0530

    update

---
+++ C Chapterwise Answers/Answers - Chapter 4/www.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float main() {
+    float a,sum = a;
+    float i;
+    printf("Type the value - ");
+    scanf("%f",&a);
+    do {
+        sum += i;
+        i++;
+    } while (i <= 10);
+    printf("The sum of the first ten numbers is %d\n", sum);
+    return 0;
+}
