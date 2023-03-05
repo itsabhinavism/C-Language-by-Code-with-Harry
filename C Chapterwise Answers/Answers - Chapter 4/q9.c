a36fd36efd5300338eef7eefc79ac9d24aa94f71
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-05 20:59:23 +0530

    update

--- C Chapterwise Answers/Answers - Chapter 4/q9.c
+++ C Chapterwise Answers/Answers - Chapter 4/q9.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 float main() {
-    float n, f = 1;
+    float i=1,n, f = 1;
     
     printf("Enter a number: ");
     scanf("%f", &n);
-    
-    float i = 1;
+
     while (i <= n) {
         f *= i;
         i++;
     }
     
-    printf("Factorial of %d is %d", n,f);
+    printf("Factorial of %f is %f", n,f);
     
     return 0;
 }
