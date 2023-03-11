af2cf3f7b7a4bd01d1277da717305875f475cff3
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-11 23:47:54 +0530

    update

--- C Chapterwise Answers/Answer - Chapter 4/q10.c
+++ C Chapterwise Answers/Answer - Chapter 4/q10.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-float main(){
-    float i,n,f=1;
-    printf("Type the value - ");
-    scanf("%f",&n);
+float main() {
+     float i,n,f=1;
+     printf("Type the value - ");
+     scanf("%f",&n);
 
-    for(i=1;i<=n;i++) 
-    // {
-    //     f=f*i;
-    // }
-
-    printf("The value of factorial %f is %f\n",n,f=f*i);
-     
+     while (i=0,i<=n) {
+   
+        printf("The factorial of %f is %f\n", n,f);f=f*i; i++;
+     }
+    
     return 0;
 }
