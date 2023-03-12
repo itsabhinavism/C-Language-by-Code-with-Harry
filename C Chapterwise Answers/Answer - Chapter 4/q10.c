3b67e05addf8091156ae2ed8a4623d42f3e5346a
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-12 12:02:11 +0530

    update

--- C Chapterwise Answers/Answer - Chapter 4/q10.c
+++ C Chapterwise Answers/Answer - Chapter 4/q10.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-float main() {
-     float i,n,f=1;
-     printf("Type the value - ");
-     scanf("%f",&n);
+float main(){
+   float i=1,n,f=1;
+   printf("Type the value - ");
+   scanf("%f",&n);
 
-     while (i=0,i<=n) {
-   
-        printf("The factorial of %f is %f\n", n,f);f=f*i; i++;
-     }
-    
-    return 0;
+   do {
+      f*=i;
+      i++;
+   }
+
+   while (i<=n);
+
+   printf("Factorial of %f is %f\n",n,f);
+   return 0;
 }
--- C Chapterwise Answers/Answer - Chapter 4/q9.c
+++ C Chapterwise Answers/Answer - Chapter 4/q9.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-float main() {
-    float i=1,n, f = 1;
-    
+float main()
+{
+    float i = 1, n, f = 1;
+
     printf("Enter a number: ");
     scanf("%f", &n);
 
-    while (i <= n) {
+    while (i <= n)
+    {
         f *= i;
         i++;
     }
-    
-    printf("Factorial of %f is %f", n,f);
-    
+
+    printf("Factorial of %f is %f\n", n, f);
+
     return 0;
 }
