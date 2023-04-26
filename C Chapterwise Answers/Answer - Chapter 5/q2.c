4a650c884b4f94645850eda7f95916a4474387b3
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-04-26 10:33:47 +0530

    update

--- C Chapterwise Answers/Answer - Chapter 5/q2.c
+++ C Chapterwise Answers/Answer - Chapter 5/q2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-float celcius(int a);
+float celcius(int a)
+{
+    float result;
+    result = (a - 32) / 1.8;
+    return result;
+}
 
-int main(){
-int a;
-    
+int main()
+{
+    int a;
+
     printf("type the value of fahrenhiet - \n");
-    scanf("%d",&a);
-    
-    printf("the value of fahrenhiet in celcius is %f",celcius(a));
+    scanf("%d", &a);
+
+    printf("the value of fahrenhiet in celcius is %f", celcius(a));
     return 0;
 }
 
-float celcius(int a) {
-    float result;
-    result = (a-32)/1.8 ;
-    return result;
-}
