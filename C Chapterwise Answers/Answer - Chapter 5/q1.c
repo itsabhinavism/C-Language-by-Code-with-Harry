ab9101ae6f79203d9bdc6ec71d15d4bc12546a70
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-04-26 13:01:52 +0530

    Update q1.c

--- C Chapterwise Answers/Answer - Chapter 5/q1.c
+++ C Chapterwise Answers/Answer - Chapter 5/q1.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+
 float average(int a, int b, int c);
 
 int main()
 {
     int a, b, c;
 
-    printf("Type a - \n");
+    printf("type a - \n");
     scanf("%d", &a);
 
-    printf("Type b - \n");
+    printf("type b - \n");
     scanf("%d", &b);
 
-    printf("Type c - \n");
+    printf("type c - \n");
     scanf("%d", &c);
 
-    printf("The average is %f",average(a,b,c));
+    printf("the average of a,b,c is %f", average(a, b, c));
 
     return 0;
 }
 
- float average(int a, int b, int c)
+float average(int a, int b, int c)
 {
     float result;
     result = (float)(a + b + c) / 3;
