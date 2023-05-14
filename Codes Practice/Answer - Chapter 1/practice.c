3e0fc69a1aeded9604dcfce6bc57fc1a41d531dd
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-05-14 20:33:21 +0530

    completed functions

--- Codes Practice/Answer - Chapter 1/practice.c
+++ Codes Practice/Answer - Chapter 1/practice.c
@@ -1,13 +1,16 @@
-// Pattern 1
 #include <stdio.h>
+
 void main()
 {
     int n;
-    printf("Enter  n: ");
+    printf("typ n - ");
     scanf("%d", &n);
+
     for (int i = 1; i <= n; i++)
+        
     {
         for (int j = 1; j <= i; j++)
+
         {
             printf("* ");
         }
